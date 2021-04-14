df6eaa710965b68e9fefeb14d2ba1dad015d219a
Naeem <Naeem@DESKTOP-1KM99FC>
2021-04-14 17:48:07 +0500

    update

--- src/app/+Case/viewschedule/viewschedule.component.ts
+++ src/app/+Case/viewschedule/viewschedule.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { UserCase } from 'src/app/model/Case.model';
 import { CaseSchedule } from 'src/app/model/CaseSchedule';
 import { CaseService } from 'src/app/service/CaseService';
 
@@ -11,20 +12,19 @@ export class ViewscheduleComponent implements OnInit {
 
   @Input() 
   public ObjSchedule: CaseSchedule= new CaseSchedule();
-
+@Input()
+public childlist:UserCase[]=[];
   //@Output() notifyCreate: EventEmitter<CaseSchedule> = new EventEmitter<CaseSchedule>();
     constructor( private caseservice:CaseService) { }
   
     ngOnInit(): void {
-      // this.caseservice.getAll().subscribe(
-      //   (res) =>{
-      //     this.ObjSchedule = res;
-      //     console.log(this.ObjSchedule);
-      //   }
-      // )
-
-      }
-    
-         
+       this.caseservice.getAll().subscribe(
+       (res) =>{
+          this.childlist = res;
+         console.log(res);
+        },
+        (error) => console.log(error)
+       )
+    } 
     }
 
