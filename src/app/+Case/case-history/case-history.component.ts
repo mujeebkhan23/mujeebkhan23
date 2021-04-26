8df8e9708c92c858f7c2cffce138d98f9ea696a0
Naeem <Naeem@DESKTOP-1KM99FC>
2021-04-27 01:25:44 +0500

    update

--- src/app/+Case/case-history/case-history.component.ts
+++ src/app/+Case/case-history/case-history.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CaseHistory } from 'src/app/model/CaseHistory';
 
 @Component({
   selector: 'app-case-history',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./case-history.component.css']
 })
 export class CaseHistoryComponent implements OnInit {
-
+@Input() casehistory: CaseHistory= new CaseHistory();
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.casehistory)
   }
 
 }
