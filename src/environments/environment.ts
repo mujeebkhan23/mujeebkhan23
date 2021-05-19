83228e5eb93b20188def5429ca70fb1d2cc3d32e
Naeem <Naeem@DESKTOP-1KM99FC>
2021-05-19 12:11:04 +0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44322/api',
-  baseUrl:'https://localhost:44322'
-//  baseUrl: 'https://appmainlawyers.azurewebsites.net',
- // apiUrl: 'https://appmainlawyers.azurewebsites.net/api',
+  // apiUrl: 'https://localhost:44322/api',
+  // baseUrl:'https://localhost:44322'
+ baseUrl: 'https://appmainlawyers.azurewebsites.net',
+  apiUrl: 'https://appmainlawyers.azurewebsites.net/api',
 };
 
 /*
