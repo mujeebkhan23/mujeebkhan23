cc0ac9d41e17e4073432b1db3567dad43787d096
mimadkhansgr@gmail.com <mimadkhansgr@gmail.com>
2021-05-31 13:58:32 +0500

    update

--- src/app/+clientprofile/list/list.component.ts
+++ src/app/+clientprofile/list/list.component.ts
@@ -2,34 +2,34 @@ import { Client } from './../../model/client.model';
 import { Component, Inject, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange, SimpleChanges, ViewChild } from '@angular/core';
 import { clientProfileService } from 'src/app/service/clientprofile';
 @Component({
-  selector: 'app-list2',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+    selector: 'Client-List',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
- 
- @Input()
-  listClient: Client[] = [];
-  disableControlList: null;
-  // @Input() disableControlList: IlkptAdjustment[];
-  constructor(   private cprofileService: clientProfileService) { }
+
+    @Input()
+    public listClient: Client[] = [];
+    disableControlList: null;
+    // @Input() disableControlList: IlkptAdjustment[];
+    constructor(private cprofileService: clientProfileService) { }
     ngOnInit() {
-        this.cprofileService.getAll().subscribe(res => {
-            this.listClient= res;
+        this.cprofileService.getAll().subscribe((res) => {
+            this.listClient = res;
             console.log(res);
         }, error => console.log(error));
-         }
- 
-//     record(per:Client)
-// {
-//   //this.objPerson=per;
-//   this.newItemEvent.emit(per);
-// }
+    }
 
-//     onSave(ownerpartner: Client): void {
-//         let item = new Client();
-//         this.listClient.push(item);
-//     }
+    //     record(per:Client)
+    // {
+    //   //this.objPerson=per;
+    //   this.newItemEvent.emit(per);
+    // }
+
+    //     onSave(ownerpartner: Client): void {
+    //         let item = new Client();
+    //         this.listClient.push(item);
+    //     }
     @Output() newItemEvent = new EventEmitter<Client>();
     @Output()
     notifySelect: EventEmitter<Client> = new EventEmitter<Client>();
@@ -37,10 +37,10 @@ export class ListComponent implements OnInit {
         this.notifySelect.emit(ownerpartner);
     }
     @Output()
-    notifyshowedit:EventEmitter<Client> = new EventEmitter<Client>();
-    showedit(){
-     this.notifyshowedit.emit();
-       
+    notifyshowedit: EventEmitter<Client> = new EventEmitter<Client>();
+    showedit() {
+        this.notifyshowedit.emit();
+
     }
     @Output()
     notifyDelete: EventEmitter<Client> = new EventEmitter<Client>();
