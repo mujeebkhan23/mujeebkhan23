da0b71957b10dea65914881266ea1598128330cd
Piccs <akhtar0316169@gmail.com>
2021-05-31 11:17:12 +0500

    update chat

--- src/app/chat-comp/list-chat/list-chat.component.ts
+++ src/app/chat-comp/list-chat/list-chat.component.ts
@@ -1,5 +1,5 @@
 import { ChatService } from './../../service/chat.service';
-import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { Chat } from 'src/app/model/chat';
 import { MessageService } from 'src/app/service/intermsgsrv';
 import { Subscription } from 'rxjs';
@@ -13,29 +13,45 @@ export class ListChatComponent implements OnInit,OnChanges {
   
   @Input()
   public listchildchat: Chat[] = [];
- public myUserId:string="";
+  public myUserId:string="";
 
-
+  public profileImage:any;
+  @ViewChild('scrollMe')
+  private myScrollContainer!: ElementRef;
 
   constructor(private chatservice: ChatService) { }
-
+  
   ngOnInit() {
-      
+   
     
 this.myUserId=  JSON.parse(localStorage.getItem('UserId') || '{}');
+this.profileImage=JSON.parse(localStorage.getItem('ImagePath') || '{}');
+
+this.scrollToBottom();
 
         }
+        ngAfterViewChecked() {        
+          this.scrollToBottom();        
+          
+      } 
+  
+      scrollToBottom(): void {
+          try {
+              this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+          } catch(err) { }                 
+      }
         ngOnChanges(changes: SimpleChanges) {
           if (changes.listchildchat && !changes.listchildchat.isFirstChange()) {
               // exteranl API call or more preprocessing...
               this.listchildchat = changes.listchildchat.currentValue;
          
           }
+          
       }
         // ngOnChanges(changes: SimpleChanges) {
         //   changes.listchildchat;
         
-        // }
+        //}
         // trackByFn(index:any) {
         //   return index; // or item.id
         // }
